0917bae5da06f51f59c648f786921689f8bc6a76
akshada <akshada.2597@gmail.com>
2025-11-09 16:24:14 +0530

    check number is prime or not

---
+++ primeNumber.js
@@ -0,0 +1,13 @@
+// Check if the number is prime
+
+function isPrime(n){
+    if(n <= 1) return true
+    for(let i =2; i<= Math.sqrt(n); i++){
+        if(n % i === 0) return false;
+    }
+    return true
+}
+
+console.log(isPrime(6)) // false
+console.log(isPrime(7)) // true
+
